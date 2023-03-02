ac8876a120d39a272725ad0cdba75c907c3d4ba9
Kason <kason.suchow@gmail.com>
2023-03-02 12:52:03 -0700

    Started work on units and trenches
    Probably putting a pin in this project for a bit, it is not nearly as enticing of a project as initially expected...

--- lib.py
+++ lib.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
 # Constants
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_HEIGHT = 800
 TITLE = "Trenches"
 
 
@@ -10,6 +11,13 @@ class Color():
     def __init__(self) -> None:
         self.BLACK = pygame.Color(0, 0, 0, 255)
         self.WHITE = pygame.Color(255, 255, 255, 255)
+        self.RED = pygame.Color(255, 0, 0, 255)
+        self.GREEN = pygame.Color(0, 255, 0, 255)
+        self.BLUE = pygame.Color(0, 0, 255, 255)
+
+    def get_random_color(self) -> pygame.Color:
+        c = pygame.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        return c
 
 color = Color()
 
